cdffb64073a41b2acb3d9b4bf784f5efe7360749
Hend <hendfouda@gmail.com>
2023-01-08 22:26:58 +0200

    adding changes to the test file to include options for the user to use the prog

--- src/com/udacity/salesinvoicegenerator/controller/TestInvoice.java
+++ src/com/udacity/salesinvoicegenerator/controller/TestInvoice.java
@@ -3,6 +3,7 @@ package com.udacity.salesinvoicegenerator.controller;
 import com.udacity.salesinvoicegenerator.model.FileOperations;
 import com.udacity.salesinvoicegenerator.model.InvoiceHeader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class TestInvoice {
@@ -15,24 +16,68 @@ public class TestInvoice {
   FileOperations fileOperations = new FileOperations();
   PrintInvoicesDetails print = new PrintInvoicesDetails();
 
-  //Ask user to enter data
-  invoicesAsReadFromUser = readData.readData();
-  //Write the data entered by the user in the CSV files
-  try {
-   fileOperations.writeFile(invoicesAsReadFromUser);
-  } catch (Exception e) {
-   e.getMessage();
-   e.printStackTrace();
+
+  //Ask if user wants to view the invoices or enter data
+  System.out.println("Do you want to:");
+  System.out.println("1- Print the invoice only? Please hit \"p key");
+  System.out.println("2- Add new invoices only? Please hit \"I key");
+  System.out.println("3- Print the invoice, then add new invoices then print them? Please hit \"A key");
+  Scanner sc=new Scanner(System.in);
+  String userInput=sc.next().toLowerCase();
+  switch (userInput){
+   case "p":
+    //Read the saved data from the CSV files
+    try {
+     invoicesAsReadFromCsv = fileOperations.readFile();
+    } catch (Exception e) {
+     e.getMessage();
+     e.printStackTrace();
+    }
+    //Print the Invoices from the CSV files
+    print.print(invoicesAsReadFromCsv);
+    break;
+   case "i":
+    //Ask user to enter data
+    invoicesAsReadFromUser = readData.readData();
+    //Write the data entered by the user in the CSV files
+    try {
+     fileOperations.writeFile(invoicesAsReadFromUser);
+    } catch (Exception e) {
+     e.getMessage();
+     e.printStackTrace();
+    }
+    break;
+   case "a":
+    //Read the saved data from the CSV files
+    try {
+     invoicesAsReadFromCsv = fileOperations.readFile();
+    } catch (Exception e) {
+     e.getMessage();
+     e.printStackTrace();
+    }
+    //Print the Invoices from the CSV files
+    print.print(invoicesAsReadFromCsv);
+    //Ask user to enter data
+    invoicesAsReadFromUser = readData.readData();
+    //Write the data entered by the user in the CSV files
+    try {
+     fileOperations.writeFile(invoicesAsReadFromUser);
+    } catch (Exception e) {
+     e.getMessage();
+     e.printStackTrace();
+    }
+    //Read the saved data from the CSV files
+    try {
+     invoicesAsReadFromCsv = fileOperations.readFile();
+    } catch (Exception e) {
+     e.getMessage();
+     e.printStackTrace();
+    }
+    //Print the Invoices from the CSV files
+    print.print(invoicesAsReadFromCsv);
+   default:
+    System.out.println("Invalid Input, please enter a valid input from the above options!");
   }
-  //Read the saved data from the CSV files
-  try {
-   invoicesAsReadFromCsv = fileOperations.readFile();
-  } catch (Exception e) {
-   e.getMessage();
-   e.printStackTrace();
-  }
-  //Print the Invoices from the CSV files
-  print.print(invoicesAsReadFromCsv);
 
  }
 }
